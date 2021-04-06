a106affef1f59abe25a974c872cb12df2ee7dcf7
ChloeArd <chloe.ardoise@gmail.com>
2021-04-06 14:51:52 +0200

    Exercice 206

--- index.php
+++ index.php
@@ -10,3 +10,61 @@
  *
  * ( PS: Sélectionnez, mais affichez le résultat à chaque fois ! ).
  */
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'live';
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    // 1
+    $request = $bdd->prepare("
+        SELECT * FROM user
+");
+
+    $request->execute();
+    echo '<pre>';
+    print_r($request->fetchAll());
+    echo "</pre><br><br>";
+
+    // 2
+    $request = $bdd->prepare("
+        SELECT * FROM article
+");
+
+    $request->execute();
+    echo '<pre>';
+    print_r($request->fetchAll());
+    echo "</pre><br><br>";
+
+    // 3
+    $request = $bdd->prepare("
+        SELECT username FROM user
+            WHERE id = ANY (SELECT user_fk FROM article WHERE contenu LIKE '%poterie')
+");
+
+    $request->execute();
+
+    foreach ($request->fetchAll() as $user_data) {
+        echo $user_data['username']. " parle de poterie dans un article <br>";
+    }
+
+    // 4
+    $request = $bdd->prepare("
+        SELECT username FROM user
+            WHERE EXISTS (SELECT * FROM article WHERE article.user_fk = user.id)
+");
+
+    $request->execute();
+    echo '<pre>';
+    print_r($request->fetchAll());
+    echo "</pre><br><br>";
+
+
+} catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
